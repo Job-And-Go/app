bb68db9161b794db4f1a8118034126bfffa79ee3
Chiheb_Eddine Mosbah <chihebeddine.mosbah@gmail.com>
2025-03-18 22:07:35 +0100

    style opti

---
+++ frontend-web/tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#3bee5e',
+        'primary-dark': '#32d951',
+      },
+    },
+  },
+  plugins: [
+    require('@tailwindcss/forms'),
+  ],
+} 
